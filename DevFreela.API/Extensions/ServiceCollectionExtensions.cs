0f3e219bb7cd06bb9db3221d54ee8ef21d932e14
henroliveira <henrique_fo@outlook.com>
2022-11-09 23:51:17 -0300

    Adicionando estrutura para a utilização do RabbitMQ no projeto

---
+++ DevFreela.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using DevFreela.Infrastructure.AuthServices;
+using DevFreela.Infrastructure.Payments;
+using DevFreela.Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ISkillRepository, SkillRepository>();
+
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IPaymentService, PaymentService>();
+            services.AddScoped<IMessageBusService, IMessageBusService>();
+
+            return services;
+        }
+    }
+}
